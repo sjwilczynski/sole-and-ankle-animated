4348e624af09824d9c8797222b2ed0dc912b3191
Stanisław <sjwilczynski@gmail.com>
2022-10-22 15:17:41 +0200

    fix grid

--- src/components/ShoeGrid/ShoeGrid.js
+++ src/components/ShoeGrid/ShoeGrid.js
@@ -1,39 +1,23 @@
-import React from 'react';
-import styled from 'styled-components/macro';
+import React from "react";
+import styled from "styled-components/macro";
 
-import SHOES from '../../data';
-import ShoeCard from '../ShoeCard';
+import SHOES from "../../data";
+import ShoeCard from "../ShoeCard";
 
 const ShoeGrid = () => {
   return (
     <Wrapper>
       {SHOES.map((shoe) => (
-        <ShoeWrapper key={shoe.slug}>
-          <ShoeCard {...shoe} />
-        </ShoeWrapper>
+        <ShoeCard {...shoe} key={shoe.slug} />
       ))}
     </Wrapper>
   );
 };
 
 const Wrapper = styled.div`
-  display: flex;
-  flex-wrap: wrap;
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
   gap: 32px;
-  /*
-    Alternatively, if we can't use 'gap', we could set:
-
-    margin: -16px;
-
-    We'd also need to set this on the ShoeWrapper:
-
-    margin: 16px;
-  */
-`;
-
-const ShoeWrapper = styled.div`
-  min-width: 275px;
-  flex: 1;
 `;
 
 export default ShoeGrid;
